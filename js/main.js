ed66f1367828decb4204ecc78d1ae7caae1ec993
berkott <berkott3002@gmail.com>
2021-09-18 13:11:45 -0400

    updated september 2021 changes, removed work experience

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 	$('#pagepiling').pagepiling({
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9',
             'page10', 'page11', 'page12', 'page13'],
-        sectionsColor: ['#314776', '#42394d', '#102877', '#0f1745', '#19356c', '#327250', '#1b3596',
-            '#7b572f', '#485013', '#2A4396', '#a22729', '#102877', '#111111'],
+        sectionsColor: ['#314776', '#5b5b5b', '#d28a22', '#0f1745', '#19356c', '#327250', '#1b3596',
+            '#7b572f', '#485013', '#2A4396', '#a22729', '#111111'],
         navigation: { //#7aa1ff
             'position': 'right',
-            'tooltips': ['Home', 'Blind Assistant', 'Techno Titans VP', 'Programming Club VP', 'Senslytics Intern',
-                'Sleep Apnea AI','Destination: Titan', 'Mole Project', 'Space Invaders', 'SmartLib', 'Titan Power Up',
+            'tooltips': ['Home', 'Covid Detection Kaggle', 'Dev Fruit', 'Blind Assistant', 'Techno Titans Robotics',
+                'Sleep Apnea AI','Destination: Titan', 'Mole Project', 'Space Invaders AI', 'SmartLib', 
                 'Techno Titans', 'The Game Palace']
         },
 
